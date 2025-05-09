2c4cfaa13142c611482c545aa2588ded7a33370c
Ile Smile <smileile9@gmail.com>
2025-05-09 11:48:34 -0700

    Added new lines of code

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,55 @@
+using System.Collections.Generic;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+      List<int> numbers = new List<int>();
+
+      int userNumber = -2;
+
+      while (userNumber != 0)
+      {
+        Console.Write("enter a number (enter 0 to quit): ");
+
+        userNumber = int.Parse(Console.ReadLine());
+
+        if (userNumber != 0)
+        {
+            numbers.Add(userNumber);
+        }
+
+      } 
+      
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        float average = 0;
+        if (numbers.Count > 0)
+        {
+            average = sum / numbers.Count;
+        }
+        else
+        {
+            Console.WriteLine("The list is empty.");
+        }
+        Console.WriteLine($"Average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (max < number)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The highest number is: {max}");
     }
 }
