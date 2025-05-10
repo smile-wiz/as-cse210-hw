6fdf1c2441b284874c59a6b84b3a836e559738aa
Ile Smile <smileile9@gmail.com>
2025-05-10 00:59:47 -0700

    Added new lines of code

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        string userName = DisplayUserName();
+        int numberSquared = NumberSquare();
+        DisplayResult(userName, numberSquared);
     }
+    static void DisplayMessage()
+    {
+        Console.WriteLine("You are welcome to the program!");
+    }
+
+    static string DisplayUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int NumberSquare()
+    {
+        Console.Write("Please eneter your favourite number: ");
+        int number = int.Parse(Console.ReadLine());
+        int numberSquared = number * number;
+
+        return numberSquared;
+    }
+
+    static void DisplayResult(string name, int numberSquared)
+    {
+        Console.WriteLine($"{name}, the square of your favourite number is: {numberSquared}.");
+    }
+    
+   
 }
